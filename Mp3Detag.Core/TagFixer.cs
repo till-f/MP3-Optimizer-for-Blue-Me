eb66f6ea3c644d3acf9698a10c5f9a6665a4d1c2
Till Fischer <till.fischer@varian.com>
2022-02-03 19:37:17 +0100

    initial commit

---
+++ Mp3Detag.Core/TagFixer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+
+namespace Mp3Detag.Core
+{
+  public enum EFileSelectionMode
+  {
+    ExplicitFile,
+    Directory,
+    DirectoryRecursive
+  }
+
+  public class TagFixer
+  {
+    private readonly string _filePath;
+
+    private readonly EFileSelectionMode _fileSelectionMode;
+
+    private string[] AllFiles
+    {
+      get
+      {
+        switch (_fileSelectionMode)
+        {
+          case EFileSelectionMode.ExplicitFile:
+            return new[] { _filePath };
+          case EFileSelectionMode.Directory:
+            return Directory.GetFiles(_filePath, "*.mp3", SearchOption.TopDirectoryOnly);
+          case EFileSelectionMode.DirectoryRecursive:
+            return Directory.GetFiles(_filePath, "*.mp3", SearchOption.AllDirectories);
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+
+      }
+    }
+
+    public TagFixer(string filePath, EFileSelectionMode fileSelectionMode)
+    {
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+      _filePath = filePath;
+      _fileSelectionMode = fileSelectionMode;
+    }
+
+    public Task RunAsync()
+    {
+      var task = new Task(Run);
+      task.Start();
+      return task;
+    }
+
+    public void Run()
+    {
+      foreach (var mp3FilePath in AllFiles)
+      {
+        Debug.Print($"Processing file {mp3FilePath}...");
+
+        var mp3File = TagLib.File.Create(mp3FilePath);
+
+        var album = mp3File.Tag.Album;
+        var artist = mp3File.Tag.FirstPerformer;
+        var title = mp3File.Tag.Title;
+
+        mp3File.RemoveTags(TagTypes.AllTags);
+
+        var id3V1 = mp3File.GetTag(TagTypes.Id3v1, true);
+
+        id3V1.Album = GetAlbumId3V1(album);
+        id3V1.Performers[0] = GetAlbumId3V1(artist);
+        id3V1.Title = GetAlbumId3V1(title);
+
+        mp3File.Save();
+      }
+    }
+
+    private static string GetAlbumId3V1(string album)
+    {
+      return SanitizeName(album, 30);
+    }
+
+    private static string GetArtistId3V1(string artist)
+    {
+      return SanitizeName(artist, 30);
+    }
+
+    private static string GetTitleId3V1(string title)
+    {
+      return SanitizeName(title, 30);
+    }
+
+    private static string SanitizeName(string fullCharsetString, int maxLength)
+    {
+      var tempBytes = Encoding.GetEncoding("ISO-8859-8").GetBytes(fullCharsetString);
+      var sanitizedString = Encoding.UTF8.GetString(tempBytes);
+      if (sanitizedString.Length > 30)
+      {
+        sanitizedString = sanitizedString.Substring(0, 30);
+      }
+      return sanitizedString;
+    }
+  }
+}
---
+++ Mp3Detag/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Mp3Detag.Core;
+using Path = System.IO.Path;
+
+namespace Mp3Detag
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void ApplyButton_Click(object sender, RoutedEventArgs e)
+    {
+      var path = DrivePath.Text;
+
+      if (string.IsNullOrWhiteSpace(path) || !Directory.Exists(path))
+      {
+        MessageBox.Show("Invalid Path");
+        return;
+      }
+
+      var tagFixer = new TagFixer(path, EFileSelectionMode.DirectoryRecursive);
+
+      tagFixer.Run();
+    }
+  }
+}
